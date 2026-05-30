414ed34f2d9898951073e97bbeefd77a46afe20a
Braian Carrizo <braiancarrizo245@gmail.com>
2026-05-30 18:57:27 -0300

    Se realizo la primera iteracion de checkout  completandolo con datos validos

--- cypress/e2e/Checkout.cy.js
+++ cypress/e2e/Checkout.cy.js
@@ -16,4 +16,21 @@ describe('Checkouts',()=>{
     cy.get(`[data-test="add-to-cart-${producto}"]`).click()
   }
 
+  it('Completar checkout con datos válidos', () => {
+    agregarProducto('sauce-labs-backpack')
+
+    
+    cy.get('[data-test="shopping-cart-link"]').click()
+    cy.get('[data-test="checkout"]').click()
+    cy.get('[data-test="firstName"]').type('Juan')
+    cy.get('[data-test="lastName"]').type('Pérez')
+    cy.get('[data-test="postalCode"]').type('5000')
+    cy.get('[data-test="continue"]').click()
+    cy.get('[data-test="finish"]').click()
+    cy.get('[data-test="complete-header"]')
+    .should('be.visible')
+    .and('contain','Thank you for your order!')
+
+  })
+
 })
