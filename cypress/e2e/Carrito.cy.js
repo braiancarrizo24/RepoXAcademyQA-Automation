1b6215efc54646166ac67121d5d6e060fb1ddbc1
Braian Carrizo <braiancarrizo245@gmail.com>
2026-05-30 16:33:51 -0300

    Se realizo la tercera y ultima iteracion del apartado del carrito de compras

--- cypress/e2e/Carrito.cy.js
+++ cypress/e2e/Carrito.cy.js
@@ -34,4 +34,22 @@ describe ('Carrito de compras sauce demo',() => {
 
 
     })
+
+    it('Eliminar un producto desde la página del carrito',()=>{
+    
+        cy.get('[data-test="username"]').type('standard_user')
+        cy.get('[data-test="password"]').type('secret_sauce')
+        cy.get('[data-test="login-button"]').click()
+        cy.get('[data-test="item-4-title-link"] > [data-test="inventory-item-name"]')
+        cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
+        cy.get('[data-test="item-0-title-link"] > [data-test="inventory-item-name"]')
+        cy.get('[data-test="add-to-cart-sauce-labs-bike-light"]').click()
+        cy.get('[data-test="shopping-cart-link"]').click()
+        cy.get('[data-test="cart-list"]')
+        cy.get('[data-test="cart-list"] > :nth-child(3)')
+        cy.get('[data-test="remove-sauce-labs-backpack"]').click()
+        cy.get('[data-test="shopping-cart-link"]')
+        cy.get('[data-test="shopping-cart-badge"]')
+
+    })
 })
